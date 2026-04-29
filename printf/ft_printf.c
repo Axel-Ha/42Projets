d188495bb197cfd67ac715c40e676c114803123f
ahalifa <ahalifa@f0r8s9.paris.42.school>
2026-04-29 18:31:07 +0200

    fait

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ahalifa <ahalifa@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 09:59:58 by ahalifa           #+#    #+#             */
-/*   Updated: 2026/04/29 12:34:48 by ahalifa          ###   ########.fr       */
+/*   Updated: 2026/04/29 18:13:06 by ahalifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,62 +20,39 @@ int	ft_putchar(char c)
 int	ft_putstr(char *str)
 {
 	int	i;
+	int	count;
 
 	i = 0;
+	count = 0;
+	if (str == NULL)
+		return (write(1, "(null)", 6));
 	while (str[i])
 	{
-		ft_putchar(str[i]);
+		count += ft_putchar(str[i]);
 		i++;
 	}
+	return (count);
 }
 
-int	ft_putnbr(int n)
-{
-	if (n == -2147483648)
-		return (write(1, "-2147483648", 11));
-	if (n < 0)
-	{
-		ft_putchar('-');
-		n *= -1;
-	}
-	if (n > 9)
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-	else
-	{
-		return (ft_putchar((n + '0')));
-	}
-}
-int	ft_unsdeci(unsigned int n)
-{
-	if (n < 0)
-		n = n + 4294967296;
-	return (ft_putnbr(n));
-}
-// TODO : 
-// Pour u && x && X quand n < 0 
-// faire 4294967296 - n pour avoir son chiffre
 int	ft_format(char c, va_list *args)
 {
 	if (c == 'c')
 		return (ft_putchar(va_arg(*args, int)));
 	else if (c == 's')
 		return (ft_putstr(va_arg(*args, char *)));
-	// else if (c == 'p')
-	// 	ft_pasDeNom3();
+	else if (c == 'p')
+		return (ft_putvoid(va_arg(*args, uintptr_t)));
 	else if (c == 'd' || c == 'i')
 		return (ft_putnbr(va_arg(*args, int)));
-	// else if (c == 'u')
-	// 	return (ft_unsdeci(va_arg(*args, unsigned int)));
+	else if (c == 'u')
+		return (ft_unsdeci(va_arg(*args, unsigned int)));
 	else if (c == 'x')
-		    return (ft_puthexa(va_arg(*args, int),c));
+		return (ft_puthexa(va_arg(*args, unsigned int), c));
 	else if (c == 'X')
-	    return (ft_puthexa(va_arg(*args, int),c));
+		return (ft_puthexa(va_arg(*args, unsigned int), c));
 	else if (c == '%')
-	    write(1, '%', 1);
-	return (-1);
+		return (ft_putchar('%'));
+	return (0);
 }
 
 int	ft_printf(const char *str, ...)
@@ -87,16 +64,19 @@ int	ft_printf(const char *str, ...)
 	va_start(args, str);
 	current = 0;
 	i = 0;
-	// if(va_start(args, str) == 0)
-	//     return (-1);
+	if (str == NULL)
+	{
+		va_end(args);
+		return (write(1, "(null)", 6));
+	}
 	while (str[i])
 	{
 		if (str[i] != '%')
-			ft_putchar(str[i]);
+			current += ft_putchar(str[i]);
 		else if (str[i + 1])
 		{
 			i++;
-			ft_format(str[i], &args);
+			current += ft_format(str[i], &args);
 		}
 		i++;
 	}
@@ -104,12 +84,19 @@ int	ft_printf(const char *str, ...)
 	return (current);
 }
 
-#include <limits.h>
-
+/*
 int	main(void)
 {
-	printf("%x\n", 14);
-	ft_printf("%x\n", 14);
+	int a = 20;
+	// printf("%%");
+	// printf("%d", printf(0));
+
+	ft_printf("%s", NULL);
+	ft_printf("%p", NULL);
+	// ft_printf("%s","");
+	// ft_printf("blabla %s\n test %d ", "Je suis un test", 20);
+	// printf("%p\n", &a);
 
 	return (0);
 }
+*/
