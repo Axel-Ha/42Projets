0c8a1d7a10597a259a8c08f33b06edadd380c02a
ahalifa <ahalifa@f0r7s10.paris.42.school>
2026-05-01 19:37:23 +0200

    du truc du rien

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ahalifa <ahalifa@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 16:10:23 by ahalifa           #+#    #+#             */
-/*   Updated: 2026/05/01 14:07:06 by ahalifa          ###   ########.fr       */
+/*   Updated: 2026/05/01 19:37:02 by ahalifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,34 @@
 
 char	*get_next_line(int fd)
 {
-	static	char *s;
-	char	*line;
+	static char	*stash;
+	char		*line;
 	char		buf[BUFFER_SIZE];
-	int		bufread;
-	bufread = 0;
-	s = "";
-	while (!(ft_strchr(s, '\n')) && (bufread = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[bufread + 1] = '\0';
-		s = ft_strjoin(s, buf);
-	}
-	//s contient toute ma phrase avant ma fin de ligne
-	// la derniere phrase n'est pas donner
-	// buffer size a 2, des caracteres sont pas lus.
-	// le programme ne s'arrete pas 
-	//bufread ne donnes pas -1 ?
-	//j'ai une boucle infini
-	line = ft_strdup(s);
-	printf("test");
-	if (bufread == -1)
-	{
-		return (NULL);
-	}
+	int			bufread;
 
+	if (BUFFER_SIZE <= 0 || read(fd, "", 0) == -1)
+		return (NULL);
+	bufread = 1;
+	if (!stash)
+		stash = ft_strdup("");
+	// bufread = read(fd, buf, BUFFER_SIZE);
+	while (!ft_strchr(stash, '\n') && bufread > 0)
+	{
+		buf[bufread] = '\0';
+		if (bufread <= 0)
+			break ;
+		stash = ft_strjoin(stash, buf);
+		bufread = read(fd, buf, BUFFER_SIZE);
+	}
+	printf("JE SUIS TEST %s",stash);
+	// buffer size a 2, des caracteres sont pas lus.
+	// le programme ne s'arrete pas
+	// stash a tout mon fichier ?
+	// je dois normalement m'arreter a \n
+	// j ai meme pas toute ma ligne hmmm
+	// je dois m'arreter a \n ou eof
+	// line retourne tout mon fichier 
+	line = ft_strdup(stash);
 	return (line);
 }
 
@@ -45,8 +49,8 @@ int	main(int ac, char **av)
 {
 	int fd = open(av[1], O_RDONLY);
 	char *line;
-	while( line = get_next_line(fd))
-		printf("%s",line);
+	while (line = get_next_line(fd))
+		printf("\n"	);
 	// free(line);
 	// close(fd);
 	return (0);
