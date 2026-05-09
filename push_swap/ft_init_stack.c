9f734ca09fae89e6566579472794e2a10a184ea5
ahalifa <ahalifa@f6r11s14.paris.42.school>
2026-05-09 20:13:28 +0200

    modif

--- push_swap/ft_init_stack.c
+++ push_swap/ft_init_stack.c
@@ -9,6 +9,8 @@ t_stack	*ft_init_stack(int **av, int pos)
 	/*check	si les chiffres dans av sont goods
 	ft_check_nbrs(int **av);
 	regarder si on est av est entre int min et int max
+	si pendant le check on a autre chose qu'un nbr return null
+	pour au final avoir un error
 	*/
 	head = NULL;
 	while (av[pos])
