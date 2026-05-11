5e33b7077335340e67531beb02f4fffb761f930d
ahalifa <ahalifa@f0r8s8.paris.42.school>
2026-05-11 15:58:23 +0200

    commentaire quoi faire

--- push_swap/ft_get_args.c
+++ push_swap/ft_get_args.c
@@ -23,7 +23,30 @@ t_flags	ft_get_args(char **av, int *start)
 	return (flags);
 }
 
-int	ft_get_nbrs(int **av, int pos)
+int	ft_check_nbrs(char **av, int pos, int ac)
 {
+	/*faire un appel deatoi sur chaque chiffre ? 
+	va bouffer des perfs de faire atoi a chaque fois ?
+	travailler comme ca pour linstant ?
+	atoi retourne le chiffre 
+	si le chiffre retourner == 0  alors c'est pas bon
+	faire une var temp, qui le prend le 1er chiffre
+	check si ce chiffre la est bien un chiffre
+
+	ensuite boucler sur av
+	check si tmp existe pas
+	len sera ac.
+	pos est notre start, on sait que les chiffres avants ont etait check, donc pas de possibilite de doublon
+
+	faire comme un union(av, tmp, pos, ac))
+
+	atoi doit check si c'est bien un chiffre
+	ensuite on fait un union pour voir si on a un doublon
+	*/
     return (0);
+}
+
+int	ft_union(char **av, char tmp, int pos, int ac)
+{
+	while(av[pos])
 }
