378c9020ae09f840ad2019f64ee86c1420767155
ahalifa <ahalifa@f1br6s12.paris.42.school>
2026-05-15 16:14:07 +0200

    feat: add init for stats

--- push_swap/ft_free_stack.c
+++ push_swap/ft_free_stack.c
@@ -1,8 +1,8 @@
 #include "push_swap.h"
 
-void	ft_free_stacks(t_stack **stack_a, t_stack **stack_b)
+void	ft_free_stacks(t_stack **stack_a, t_stack **stack_b, t_stats *stats)
 {
 	ft_stack_clear(stack_a);
 	ft_stack_clear(stack_b);
-		
+	free(stats);
 }
---
+++ push_swap/ft_init.c
@@ -0,0 +1,45 @@
+#include "push_swap.h"
+
+t_stack	*ft_init_stack(char **args)
+{
+	int		i;
+	t_stack	*head;
+	t_stack	*new;
+
+	head = NULL;
+	i = 0;
+	while (args[i])
+	{
+		new = ft_listnew(ft_atoi(args[i]));
+		if (!new)
+		{
+			ft_stack_clear(&head);
+			return (NULL);
+		}
+		ft_add_back(&head, new);
+		i++;
+	}
+	return (head);
+}
+
+t_stats	*ft_init_stats(void)
+{
+	t_stats *stats;
+	stats = malloc(sizeof(t_stats));
+	if(!stats)
+		return (NULL);
+	stats->total_ops = 0;
+	stats->disorder_metric = 0;
+	stats->pa = 0;
+	stats->pb = 0;
+	stats->sa = 0;
+	stats->sb = 0;
+	stats->ss = 0;
+	stats->ra = 0;
+	stats->rb = 0;
+	stats->rr = 0;
+	stats->rra = 0;
+	stats->rrb = 0;
+	stats->rrr = 0;
+	return (stats);
+}
