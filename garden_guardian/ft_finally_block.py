5b05f547ff6812a110171393cd8ece7f55dfb0b9
ahalifa <ahalifa@f4r6s9.paris.42.school>
2026-06-01 15:02:49 +0200

    garden_guardian fait

---
+++ garden_guardian/ft_finally_block.py
@@ -0,0 +1,61 @@
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown garden error"):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return f"{self.message}"
+
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown plant error"):
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message: str = "Unknown water error"):
+        super().__init__(message)
+
+
+def water_plant(plant_name: str):
+    if plant_name != plant_name.capitalize():
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+    else:
+        print(f"Watering {plant_name}: [OK]")
+
+
+def test_watering_system() -> None:
+    print("=== Garden Watering System ===")
+    print()
+
+    print("Testing valid plants...")
+    try:
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("Lettuce")
+        water_plant("Carrots")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        return
+    finally:
+        print("Closing watering system")
+
+    print()
+    print("Testing invalid plants...")
+    try:
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("lettuce")
+        water_plant("Carrots")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        print("ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+
+
+if __name__ == "__main__":
+    test_watering_system()
+    print()
+    print("Cleanup always happens, even with errors!")
---
+++ garden_guardian/ft_raise_exception.py
@@ -0,0 +1,60 @@
+def input_temperature(temp_str: str) -> int:
+    num: int = int(temp_str)
+    if num > 40:
+        raise ValueError(f"{num}°C is too hot for plants"
+                         " (max 40)°C")
+    elif num < 0:
+        raise ValueError(f"{num}°C is too cold for plants"
+                         " (min 0)°C")
+    return num
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    print()
+
+    print("Input data is '25'")
+    try:
+        res: int = input_temperature("25")
+        print(f"Temperature is now {res}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+    print()
+
+    print("Input data is 'abc'")
+    try:
+        res_invalid: int = input_temperature("abc")
+        print(f"Temperature is now {res_invalid}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+    print()
+    print("All tests completed - program didn't crash!")
+
+    print()
+
+    print("Input data is '100'")
+    try:
+        res_invalid: int = input_temperature("100")
+        print(f"Temperature is now {res_invalid}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+    print()
+    print("All tests completed - program didn't crash!")
+
+    print()
+
+    print("Input data is '-50'")
+    try:
+        res_invalid: int = input_temperature("-50")
+        print(f"Temperature is now {res_invalid}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+    print()
+    print("All tests completed - program didn't crash!")
+
+
+test_temperature()
