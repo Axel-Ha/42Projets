b4447c70c6809f848bac87be62697eace41ef172
ahalifa <ahalifa@f1r12s10.paris.42.school>
2026-06-05 14:41:27 +0200

    mypy fait

--- growing_code/ft_count_harvest_recusrvise.py
+++ growing_code/ft_count_harvest_recusrvise.py
@@ -1,4 +1,4 @@
-def ft_print_harvest(days, max_days):
+def ft_print_harvest(days: int, max_days: int) -> None:
     if days != max_days:
         print("Days", days)
         ft_print_harvest(days + 1, max_days)
@@ -7,6 +7,6 @@ def ft_print_harvest(days, max_days):
         print("Harvest time!")
 
 
-def ft_count_harvest_recursive():
+def ft_count_harvest_recursive() -> None:
     harvest = int(input("Days until harvest: "))
     ft_print_harvest(1, harvest)
