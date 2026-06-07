1e199fd214a9c2560bcf2602293de52bc7c5640a
Axel Ha <47282497+Axel-Ha@users.noreply.github.com>
2026-06-07 18:45:48 +0200

    maj arborescence projet

--- push_swap/utils/ft_get_args.c
+++ push_swap/utils/ft_get_args.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 t_flags	*ft_get_flags(char **args, int *start, t_flags *flag)
 {
