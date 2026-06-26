1d3aff6a51719167e3c38ae0cb9d9da3b1e2a39b
ahalifa <ahalifa@f0r3s20.paris.42.school>
2026-06-26 13:04:00 +0200

    exo presque finis

---
+++ Python/funcmage/ex3/functools_artifacts.py
@@ -0,0 +1,79 @@
+from functools import reduce, partial, lru_cache, singledispatch
+from collections.abc import Callable
+import operator
+from typing import Any
+
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+    if not spells:
+        return 0
+    match operation:
+        case "add":
+            return reduce(operator.add, spells)
+        case "multiply":
+            return reduce(operator.mul, spells)
+        case "max":
+            return reduce(max, spells)
+        case "min":
+            return reduce(min, spells)
+        case _:
+            raise ValueError("Wrong operator")
+
+
+def enchantment(power: int, element: str, target: str) -> str:
+    return f"power: {power}, element: {element}, target: {target}"
+
+
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+    return {'Ice': partial(base_enchantment, 50, "Ice"),
+            'Wind': partial(base_enchantment, 50, "Wind"),
+            'Fire': partial(base_enchantment, 50, "Fire")}
+
+
+@lru_cache()
+def memoized_fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+    return memoized_fibonacci(n-1) + memoized_fibonacci(n-2)
+
+
+def spell_dispatcher() -> Callable[[Any], str]:
+    return dispatcher
+
+
+@singledispatch
+def dispatcher(spell: Any) -> str:
+    return "Unknown spell type"
+
+
+@dispatcher.register(int)
+def _1(damage: int) -> str:
+    return f"{damage} damage"
+
+
+@dispatcher.register(str)
+def _2(enchantment: str) -> str:
+    return f"{enchantment}"
+
+
+@dispatcher.register(list)
+def _3(multi_cast: list) -> str:
+    return f"{len(multi_cast)} spells"
+
+
+def main() -> None:
+    spell_powers = [44, 11, 20, 15, 32, 37]
+    operations = ['add', 'multiply', 'max', 'min']
+    print(spell_reducer(spell_powers, operations[2]))
+    partial_test = partial_enchanter(enchantment)
+    print(partial_test['Fire']("Wizard"), partial_test['Ice']("Wizard"))
+    fibo = memoized_fibonacci(18)
+    print(fibo)
+    print(memoized_fibonacci.cache_info())
+
+    dispatch = spell_dispatcher()
+    print(dispatch({"Fire": "yeah"}))
+
+
+if __name__ == "__main__":
+    main()
